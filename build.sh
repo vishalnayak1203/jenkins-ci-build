7f4450c871a1906d840510f85ff501ce6dac96c0
Vishal Nayak <Vishal Nayak>
2018-08-01 15:18:54 +0530

    Old folder

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p builds
+mkdir -p builds/old
 BUILD_EPOCH_TS=$(date +%s) # Seconds
 BUILD_VERSION=jenkins-ci-${BUILD_EPOCH_TS}
 
-tar -pczf ${WORKSPACE}/builds/${BUILD_VERSION}.tar.gz ${WORKSPACE} --exclude "${WORKSPACE}/builds"
+find builds/ -maxdepth 1 -type f -print0 | xargs -0 mv -t builds/old
+
+tar -pczf builds/${BUILD_VERSION}.tar.gz . --exclude "/builds" --exclude "/.git"
