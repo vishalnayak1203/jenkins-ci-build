b037ce62215db397a0482f2b8f8c1e4bc201d3a8
Vishal Nayak <Vishal Nayak>
2018-08-01 15:00:09 +0530

    Bug Fixes

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p builds
-export BUILD_EPOCH_TS = $(date +%s) # Seconds
-BUILD_VERSION = jenkins-ci-${BUILD_EPOCH_TS}
-export BUILD_VERSION
+BUILD_EPOCH_TS=$(date +%s) # Seconds
+BUILD_VERSION=jenkins-ci-${BUILD_EPOCH_TS}
 
 tar -pczf builds/${BUILD_VERSION}.tar.gz ../ --exclude "../builds"
