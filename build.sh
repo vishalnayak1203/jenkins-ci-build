466b48c1f3c1f172febdf737c0cc037cc38342ee
Vishal Nayak <Vishal Nayak>
2018-08-01 15:10:53 +0530

    sudo removed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p builds
+mkdir -p builds
 BUILD_EPOCH_TS=$(date +%s) # Seconds
 BUILD_VERSION=jenkins-ci-${BUILD_EPOCH_TS}
 
-sudo tar -pczf ${WORKSPACE}/builds/${BUILD_VERSION}.tar.gz ${WORKSPACE} --exclude "${WORKSPACE}/builds"
+tar -pczf ${WORKSPACE}/builds/${BUILD_VERSION}.tar.gz ${WORKSPACE} --exclude "${WORKSPACE}/builds"
