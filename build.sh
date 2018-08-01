74d86c7e45fca857d45cf87e9db7a45db649074a
Vishal Nayak <Vishal Nayak>
2018-08-01 16:10:06 +0530

    Build fix 5

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ BUILD_VERSION=jenkins-ci-${BUILD_EPOCH_TS}
 
 find builds/ -maxdepth 1 -type f -print0 | xargs -0 mv -t builds/old
 
-tar --exclude 'builds' --exclude '.git*' -pczf builds/${BUILD_VERSION}.tar.gz . --transform 's,./,'${BUILD_VERSION}'/,'
+tar --exclude 'builds' --exclude '.git*' -pczf builds/${BUILD_VERSION}.tar.gz . --transform 's,^./,'${BUILD_VERSION}'/,'
