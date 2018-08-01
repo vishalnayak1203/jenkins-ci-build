6d33a6da2bc032fb6e1fab9fc2e6cdeaf0253f76
Vishal Nayak <Vishal Nayak>
2018-08-01 15:08:34 +0530

    Workspace path added

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p builds
+sudo mkdir -p builds
 BUILD_EPOCH_TS=$(date +%s) # Seconds
 BUILD_VERSION=jenkins-ci-${BUILD_EPOCH_TS}
 
-tar -pczf builds/${BUILD_VERSION}.tar.gz ../ --exclude "../builds"
+sudo tar -pczf ${WORKSPACE}/builds/${BUILD_VERSION}.tar.gz ${WORKSPACE} --exclude "${WORKSPACE}/builds"
