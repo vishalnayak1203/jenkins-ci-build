d73f7bbbee57d2aa31940ffb952bb4c0e711a4e7
Vishal Nayak <Vishal Nayak>
2018-08-06 15:14:37 +0530

    Custom Artifact

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 mkdir -p builds/old
+
+if [ -n "${CHANGE_ID}" ] ; then
+  # do something because it's a pull request
+  ARTIFACT_NAME=jenkins-ci-nightly
+else
+  # not a pull request
+  ARTIFACT_NAME=jenkins-ci-release
+fi
+
 BUILD_EPOCH_TS=$(date +%s) # Seconds
-BUILD_VERSION=jenkins-ci-${BUILD_EPOCH_TS}
+BUILD_VERSION=${ARTIFACT_NAME}-${BUILD_EPOCH_TS}
 
 find builds/ -maxdepth 1 -type f -print0 | xargs -0 mv -t builds/old
 
