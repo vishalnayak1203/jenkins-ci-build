0115fad03171ce988c6b3b387e1e7438d68ff8b1
Vishal Nayak <Vishal Nayak>
2018-08-09 11:24:51 +0530

    Tests Failing

--- tests.sh
+++ tests.sh
@@ -2,4 +2,12 @@
 
 export ENV=TEST
 python -m unittest unit_tests
+exit_code=$?
+if [ $exit_code -eq 0 ]
+then
+  echo "Continued INtegration Tests"
+else
+  echo "Exited with code:${exit_code}"
+  exit $exit_code
+fi
 
